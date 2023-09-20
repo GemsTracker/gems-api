cd5153c7a5467667721ddd8207781e28117a7661
Jasper van Gestel <jvangestel@gmail.com>
2023-09-20 10:37:58 +0200

    Add delete model events

---
+++ src/Event/DeleteFailedModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Gems\Api\Event;
+
+class DeleteFailedModel extends ModelEvent
+{
+    
+}
---
+++ src/Event/DeleteModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Gems\Api\Event;
+
+class DeleteModel extends ModelEvent
+{
+
+}
