d0322a89f3da58d47f6d99cfd6a7d276dac67b9f
Jasper van Gestel <jvangestel@gmail.com>
2024-09-05 15:38:34 +0200

    Static fix

--- src/Handlers/TestModelRestHandler.php
+++ src/Handlers/TestModelRestHandler.php
@@ -14,6 +14,7 @@ class TestModelRestHandler extends ModelRestHandler
     public function getOne(mixed $id, ServerRequestInterface $request): ResponseInterface
     {
         $idField = $this->getIdField();
+        $filter = [];
         if ($idField) {
             $filter = $this->getIdFilter($id, $idField, $request);
 
