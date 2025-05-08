5285de1ceb05f9a1c94b604844dc55fe2406a084
Jasper van Gestel <jvangestel@gmail.com>
2025-05-08 17:31:12 +0200

    Add extra exception to ApiRequestExceptionMiddleware

--- src/Middleware/ApiRequestExceptionMiddleware.php
+++ src/Middleware/ApiRequestExceptionMiddleware.php
@@ -11,22 +11,27 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Symfony\Component\Serializer\Exception\ExceptionInterface as SerializerExceptionInterface;
+use Symfony\Component\Serializer\Exception\MissingConstructorArgumentsException;
 
 class ApiRequestExceptionMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
-            $response = $handler->handle($request);
-            return $response;
+            return $handler->handle($request);
         } catch (SymfonyValidatorException $e) {
             return new JsonResponse([
                 'error' => 'validation_error',
                 'message' => $e->getFormattedViolations(),
             ], 422);
+        } catch(MissingConstructorArgumentsException $e) {
+            return new JsonResponse([
+                'error' => 'input_error',
+                'message' => sprintf('missing required input variables: %s', join(', ', $e->getMissingConstructorArguments())),
+            ], 422);
         } catch (SerializerExceptionInterface $e) {
             return new JsonResponse([
-                'error' => 'validation_error',
+                'error' => 'input_error',
                 'message' => $e->getMessage(),
             ], 422);
         }
